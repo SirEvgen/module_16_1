98c7ce13a90093300aecde25a9ba80731da15f56
SirEvgen <sandrofa6666666@gmail.com>
2024-11-20 18:17:22 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -4,18 +4,18 @@ app = FastAPI()
 
 
 @app.get("/")
-async def home() -> str:
+async def get_main_page() -> str:
     return ("Главная страница")
 
 @app.get("/user/admin") #/user/admin
-async def admin() -> str:
+async def get_admin_page() -> str:
     return ("Вы вошли как администратор")
 
 @app.get("/id") # /id?user_id=123
-async def id_paginator(user_id: int) -> str:
+async def get_user_number(user_id: int) -> str:
     return (f"Вы вошли как пользователь № {user_id}")
 
-@app.get("/user/{username}/{age}") # /user/alex/33
-async def welcome_page(username: str, age: int) -> str:
+@app.get("/user") # /user?username=alex&age=33
+async def get_user_info(username: str, age: int) -> str:
     return (f"Информация о пользователе. Имя: {username}, Возраст: {age}")
 
